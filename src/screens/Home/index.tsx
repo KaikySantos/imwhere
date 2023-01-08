0d9a1f4ea8779fa9095e1ada22a0f43be51c2c0d
kaikySantos <info.kaiky@gmail.com>
2023-01-07 21:55:54 -0300

    initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+
+export default function Home() {
+  function handleParcipantAdd() {
+    console.log("KAIKYYYYYYYYYYYYYYYYYY")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleParcipantAdd}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
